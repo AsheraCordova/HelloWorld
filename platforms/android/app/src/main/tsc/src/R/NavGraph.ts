aed704ef25ec1401fbd8b6cff5fb18509baf243e
Ashera Cordova <ashera.cordova@gmail.com>
2022-10-15 08:53:45 +0530

    Deleted platforms and plugin directory

--- platforms/android/app/src/main/tsc/src/R/NavGraph.ts
+++
@@ -1,4 +0,0 @@
-export const action_error_to_error_detail:string = 'action_error_to_error_detail#@layout/error_detail';
-export const index:string = 'fragment#index#layout/index.xml';
-export const error:string = 'fragment#error#layout/error.xml';
-export const error_detail:string = 'fragment#error_detail#layout/error_detail.xml';
